db7929a1598f67a7d754ec80ea40297991d90894
RaaaiY <161962361+RaaaiY@users.noreply.github.com>
2024-03-02 13:48:55 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,39 @@ import streamlit as st
 
 
 def main():
-    st.write("Hello World!")
-
+    # Title
+    st.title('Meal Planner')
+    # Header
+    st.header('Header')
+    # Text
+    st.text('Some text')
+    # SubHeader
+    st.subheader('Sub header')
+    # MarkDownText
+    st.markdown('**Markdown is available **')
+    # LaTeX Text
+    st.latex(r'\bar{X} = \frac{1}{N} \sum_{n=1}^{N} x_i')
+    # Code
+    st.code('print(\'Hello, World!\')')
+    # ErrorMessage
+    st.error('Error message')
+    # WarningMessage
+    st.warning('Warning message')
+    # InformationMessage
+    st.info('Information message')
+    # SuccessMessage
+    st.success('Success message')
+    # Exception
+    st.exception(Exception('Oops!'))
+    # Dictionary
+    d = {
+        'foo': 'bar',
+        'users': [
+            'alice',
+            'bob',
+        ],
+    }
+    st.json(d)
 
 if __name__ == "__main__":
     main()
