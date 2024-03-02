4e858958c5b422e6d16620d1bc1c2c403c589ef4
RaaaiY <161962361+RaaaiY@users.noreply.github.com>
2024-03-02 13:23:30 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,1 @@
-# app.py
 
-import streamlit as st
-
-# Add Title
-st.title('NutritionPlanner')
-
-# Add Text
-st.write('Text')
-
-# Show Data
-df = pd.DataFrame({
-    'Line1': [1, 2, 3, 4],
-    'Line2': [10, 20, 30, 40]
-})
-st.write(df)
