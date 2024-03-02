44a6f8e3bea7383abca5209f53690d72cb07288a
RaaaiY <161962361+RaaaiY@users.noreply.github.com>
2024-03-02 13:58:42 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,31 +13,13 @@ def main():
     st.text('Some text')
     # SubHeader
     st.subheader('Sub header')
-    # MarkDownText
-    st.markdown('**Markdown is available **')
-    # LaTeX Text
-    st.latex(r'\bar{X} = \frac{1}{N} \sum_{n=1}^{N} x_i')
-    # Code
-    st.code('print(\'Hello, World!\')')
-    # ErrorMessage
-    st.error('Error message')
-    # WarningMessage
-    st.warning('Warning message')
-    # InformationMessage
-    st.info('Information message')
-    # SuccessMessage
-    st.success('Success message')
-    # Exception
-    st.exception(Exception('Oops!'))
-    # Dictionary
-    d = {
-        'foo': 'bar',
-        'users': [
-            'alice',
-            'bob',
-        ],
-    }
-    st.json(d)
+    #Button
+    st.button('Hit me')
+    #TextInput
+    st.text_input('Enter some text')
+    #NumberInput
+    st.number_input('Enter a number')
+   
 
 if __name__ == "__main__":
     main()
